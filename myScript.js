37d37eb227ab59e932941f2bc27a1893648324f0
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-09-25 20:19:16 -0500

    completed much of the button logic for equals and displaying on-screen digits, as well as clearing the screen

--- myScript.js
+++ myScript.js
@@ -1,19 +1,95 @@
-function add(){
+let num1 = 0;
+let num2 = 0;
+let operator = "none";
+let screen = document.body.querySelector('.calculator-screen');
 
+
+function isCharacterANumber(char){
+  return /^[0-9]*$/.test(char);
 }
 
-function subtract(){
-
+function isButtonAnOperator(char){
+  if(char === "+" || char === "-" || char === "/" || char ==="*") return true;
+  return false;
 }
 
-function multiply(){
-
+function add(num1, num2){
+  return num1 + num2;
 }
 
-function divide(){
-
+function subtract(num1, num2){
+  return num1 - num2;
 }
 
-function operate(num1, operator, num2){
-  
+function multiply(num1, num2){
+  return num1 * num2;
 }
+
+function divide(num1, num2){
+  return num1/num2;
+}
+
+function operate(num1, num2, operator){
+  let result = 0;
+
+  if(operator === "+")
+    result = add(num1, num2);
+  else if(operator === "-")
+    result = subtract(num1, num2);
+  else if(operator === "*")
+    result = multiply(num1, num2);
+  else if(operator === "/")
+    result = divide(num1, num2);
+
+  console.log(result);
+  return result;
+}
+
+  // format: num1OperatorNum2 -> (global-variables) num1, num2, operator
+function parseNumsAndOperator(str){
+  let idxForOperator = -1;
+  // find the operator firsst
+  for(let i=0; i<str.length; i++){
+    if(str[i] === "+" || str[i] === "-" ||
+    str[i] === "/" || str[i] === "*"){
+      idxForOperator = i;
+      break;
+    }
+  }
+  operator = str.charAt(idxForOperator);
+  num1 = +(str.substring(0, idxForOperator));
+  num2 = +(str.substring(idxForOperator+1));
+}
+
+function buttonLogic(){
+  let calculatorButtons = Array.from(document.body.querySelectorAll('.buttons-row-flex-container .button'));
+
+  calculatorButtons.forEach((button) => {
+    // handle screen-content
+    if(isCharacterANumber(button.textContent) || isButtonAnOperator(button.textContent) ||
+    button.textContent === '.'){
+      button.addEventListener('click', () => {
+        screen.textContent += button.textContent;
+      });
+    }
+    else if(button.textContent === 'C'){
+      button.addEventListener('click', () => {
+        screen.textContent = "";
+      });
+    }
+    // handle computation
+    else if(button.textContent === "="){
+      button.addEventListener('click', () =>{
+        parseNumsAndOperator(screen.textContent);
+        console.log(num1, num2, operator);
+        operate(num1, num2, operator);
+      });
+    }
+  });
+}
+
+function main(){
+  buttonLogic(screen);
+}
+
+main();
